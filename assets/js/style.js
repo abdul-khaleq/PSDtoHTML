32970ab1e3532d3123e28b4c54c33f7cb87507b3
abdul-khaleq <2191011069@uttarauniversity.edu.bd>
2021-07-03 09:02:01 +0600

    first commit

---
+++ assets/js/style.js
@@ -0,0 +1,17 @@
+// //mixup
+ var containerEl = document.querySelector('.mix-up-padding');
+ var mixer = mixitup(containerEl);
+
+ 
+    jQuery(document).ready(function($){
+        $(".owl-cousol-area").owlCarousel({
+            items: 1,
+            loop: true,
+            dots: true,
+            nav: true,
+            navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
+            autoplay: false,
+         });
+        
+     });
+
